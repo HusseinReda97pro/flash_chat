4160d70e437ed7783208d884463765b04dd92bc4
HusseinReda97pro <pro.hussein.reda@gmail.com>
2024-09-25 07:29:47 +0300

    first commit

---
+++ Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Hussein Reda on 25/09/2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+
+struct Message {
+    let sender: String
+    let body: String
+}
